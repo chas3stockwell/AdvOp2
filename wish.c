60e7ca601cd9069b2e0d51550ed0bd05415ab44a
chas3stockwell <cx1stockwell@comcast.net>
2020-09-29 22:09:00 -0700

    10test completed

--- wish.c
+++ wish.c
@@ -75,6 +75,7 @@ int cdcommand(char buffer[]){
             if( cd(tok) == -1){
                 errorMessage(); 
             }else{
+                
                 cd(tok);
             }
             
@@ -92,9 +93,31 @@ int cdcommand(char buffer[]){
     return 0; 
 }
 
+void passIntoFile(char buffer[], int count){
+    
+    char *end, *r, *tok;
+    r = end = strdup(buffer);
+    int curr_counter  = 0; 
+    bool IsPath = false;
+ 
+    while ((tok = strsep(&end, " ")) != NULL) {
+        curr_counter += 1;
+    }
+
+    if( (curr_counter - count) != 1 ){
+        errorMessage(); 
+    }
+    
+    /*else if((curr_counter - count) > 1){
+        errorMessage(); 
+    }*/
+        
+}
+
+
 
 int lscommand(char buffer[]){
-    //printf("%s", buffer);
+    
     int count = 0;
 
     char *end, *r, *tok;
@@ -115,8 +138,14 @@ int lscommand(char buffer[]){
 
 
         if (IsPath){
+
             
-            
+            if( (strcmp(tok, ">\n") == 0) || (strcmp(tok, ">") == 0) ){
+                passIntoFile(buffer, count);
+                
+            }
+
+
             char new_string[50];
             strcpy(new_string,  "/usr");
             char string_ss[50];
@@ -150,7 +179,10 @@ int lscommand(char buffer[]){
         char *args[2];
         args[0] = "/bin/ls";        
         args[1] = NULL;
-        execv(args[0], args);
+        if ( execv(args[0], args) == -1 ) {
+            errorMessage(); 
+        }
+        errorMessage(); 
     }
     
 
@@ -173,7 +205,7 @@ int pathcommand(char buffer[]){
             
             //char new_path[50];
             //appendChar(tok, "/", strlen(tok)-1);
-
+            removeNewline(tok); 
             strcpy(Path,  tok);
            
 
@@ -294,29 +326,42 @@ int argwishLoop(FILE *fp){
                 strcpy(strcommand, Path);
                 appendChar(strcommand, "/", strlen(strcommand));
 
-                strcat(strcommand, tok);
-
-                pid_t pid;
-                char *const parmList[] = {"/bin/sh", strcommand,  NULL};
                 
 
+                strcat(strcommand, tok);
+                removeNewline(strcommand);
                 
                 if (access(strcommand, X_OK) == -1 ){
-                    errorMessage(); 
+                    //essentially, if there are other commands left
+                    if(strcmp(Path, "") == 0){
+                       fork();
+                    }
+                    
+                    errorMessage();
+                    
                 }
+                
+                
 
-                pid = fork(); 
-                if (pid == -1) { 
+                
+                char *const parmList2[] = {"/bin/sh", strcommand,  NULL};
+                
+                
+
+                pid_t pid;
+                
+                if ((pid = fork()) == -1) { 
                     errorMessage();
                         
                      
                 }else if (pid == 0) {
-                    execv("/bin/sh", parmList);
-                                  
                     
-                }
-
+                        execv("/bin/sh", parmList2);    
+                        errorMessage();  
+                    
                 
+                                      
+                }
 
             }
             
